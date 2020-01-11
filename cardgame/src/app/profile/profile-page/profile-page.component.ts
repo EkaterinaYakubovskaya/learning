a2d47a6429295c914af2f221054b419d71c48ca5
Ekaterina <ekaterinayakubovska@gmail.com>
2020-01-11 15:40:53 +0200

    profile form

--- cardgame/src/app/profile/profile-page/profile-page.component.ts
+++ cardgame/src/app/profile/profile-page/profile-page.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  helloMsg: string;
+  age: number;
+  gender: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.helloMsg = 'Hello good world!';
+  }
+
+  calculateAge() {
+    this.age =
+     new Date().getFullYear() - (+this.birthday.substr(this.birthday.lastIndexOf('/')+1));
   }
 
 }
--- cardgame/src/app/profile/profile.module.ts
+++ cardgame/src/app/profile/profile.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProfilePageComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FormsModule
   ],
   exports: []
 })
